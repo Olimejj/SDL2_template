57b10de2780b9f268cf15985241d5fc9d4e3aae3
J.J. Oliphant <olimejj@gmail.com>
2024-01-26 06:52:22 -0700

    first commit

---
+++ First_Window_Project/src/main.cpp
@@ -0,0 +1,5 @@
+//Our setup for now is a bit unique so here is an example of how to include the SDL libraries in our projects
+#define SDL_MAIN_HANDLED
+#include "..\..\SDL2\headers\SDL.h"
+#include "..\..\SDL2\headers\SDL_image.h"
+
