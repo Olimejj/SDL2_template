b45198951df2f26f11ca2befc678f6c3d1a93a74
J.J. Oliphant <olimejj@gmail.com>
2024-02-29 14:39:00 -0700

    restructure to be an out of the way sdl template.

--- First_Window_Project/src/main.cpp
+++ First_Window_Project/src/main.cpp
@@ -1,5 +1,86 @@
 //Our setup for now is a bit unique so here is an example of how to include the SDL libraries in our projects
 #define SDL_MAIN_HANDLED
 #include "..\..\SDL2\headers\SDL.h"
-#include "..\..\SDL2\headers\SDL_image.h"
+//#include "..\..\SDL2\headers\SDL_image.h"
 
+#include <iostream>
+
+using namespace std;
+
+namespace SDL_globals{
+    const int SCREEN_WIDTH = 1200;
+    const int SCREEN_HEIGHT = 800;
+
+    //The window we'll be rendering to
+    SDL_Window* window = NULL;
+
+    //surface contained by the window
+    SDL_Surface* screen_surface = NULL;
+
+    //image surfaces
+    SDL_Surface* background_surface = NULL;
+    SDL_Surface* image_surface = NULL;
+}
+using namespace SDL_globals;
+
+int init_window();
+void game_loop();
+void draw_images();
+
+
+
+int main(int argc, char* args[]){
+    cout<< "main is running" << endl;
+
+    init_window();
+    draw_images();
+
+    game_loop();
+
+
+
+    return 0;
+}
+
+int init_window(){
+    int error = SDL_Init(SDL_INIT_VIDEO);
+    if(error < 0){
+        cout << "SDL Init returned error code: " << SDL_GetError() << endl;
+        return error;
+    }
+    //window maker
+    window = SDL_CreateWindow("Animal Farm", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+
+    if(window == NULL){
+        cout << "Window could not be created! SDL_Error: " << SDL_GetError() << endl;
+        return -2;
+    }
+
+    screen_surface = SDL_GetWindowSurface(window);
+    return error;
+}
+
+void game_loop(){
+    SDL_Event e;
+    bool quit = false;
+
+    while(quit == false){
+        while(SDL_PollEvent(&e)){
+            if(e.type == SDL_QUIT){
+                quit = true;
+            }
+
+        }
+    }
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+}
+
+void draw_image(string image_path, SDL_Surface* image_surface){
+    image_surface = SDL_LoadBMP(image_path.c_str());
+
+    SDL_BlitSurface(image_surface, NULL, screen_surface, NULL);
+
+    SDL_UpdateWindowSurface(window);
+}
